dc1aac46e311ccabe69cc9d23123268a0c15082c
Julius Cecilia <juliuscecilia33@gmail.com>
2026-09-19 02:22:14 -0500

    demo

--- src/components/demo/tapestryacademy/TapestryAcademyWebsiteDashboardDemo.tsx
+++ src/components/demo/tapestryacademy/TapestryAcademyWebsiteDashboardDemo.tsx
@@ -2,9 +2,19 @@
 
 import WebsiteDashboardDemo from "@/components/sections/WebsiteDashboardDemo";
 import { tapestryAcademyConfig } from "@/data/school-demos/tapestry-academy";
+import type { ComponentProps } from "react";
 
-export default function TapestryAcademyWebsiteDashboardDemo() {
+type Props = ComponentProps<typeof WebsiteDashboardDemo>;
+
+export default function TapestryAcademyWebsiteDashboardDemo({
+  disableTour = true,
+  ...props
+}: Props) {
   return (
-    <WebsiteDashboardDemo config={tapestryAcademyConfig} disableTour={true} />
+    <WebsiteDashboardDemo
+      config={tapestryAcademyConfig}
+      disableTour={disableTour}
+      {...props}
+    />
   );
 }
